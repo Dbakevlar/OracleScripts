132dc95588d2e51afe19455d8c2e6711ca30d8ac
Kellyn Gorman <dbakevlar@gmail.com>
2025-04-14 16:35:49 -0700

    Add files via upload

---
+++ chk_redo_wts.sql
@@ -0,0 +1,6 @@
+ SELECT name, value
+   FROM SYS.v_$sysstat
+ WHERE NAME in ('redo buffer allocation retries',
+                'redo log space wait time');
+
+
---
+++ dyn_kill_script.sql
@@ -0,0 +1,6 @@
+--Dynamically generate SQL when required to kill ton of sessions:
+select 'alter system kill session '''||u.sid||','||u.serial#||''';' from v$session u
+where u.type='USER'
+and u.module like '%<module>%;
+
+
